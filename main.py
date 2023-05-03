caba8f7439a8ee49aa69a3144ce84ccb34b3ffe4
Noah Bumgardner <noah@playgamecafe.com>
2023-05-03 00:04:14 -0500

    Encapsulate All Data Into Game_Instance

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
+class Game_Instance(object):
+    def __init__(self):
+        self._game_title = 'One Step From Eden'
+    
+    def get_game_title(self):
+        return self._game_title
+    
+    def get_prompt_is_game_open(self):
+        return 'Is {} open? y/n'.format(self._game_title)
+    
+    def get_retry_message_when_game_is_closed(self):
+        return 'Open the game {}, then try again.'.format(self._game_title)
+    
+    def get_start_message_when_game_is_open(self):
+        return 'Watch me play {}.'.format(self._game_title)
+    
+    def is_response_yes(self, response):
+        return response.lower() != 'y'
+
 def main():
-    game_title = 'One Step From Eden'
+    # Copy over values from game properties.
+    game_instance = Game_Instance()
 
-    response = input('Is {} open? y/n'.format(game_title))
+    prompt_message = game_instance.get_prompt_is_game_open()
+    response = input(prompt_message)
 
-    if response.lower() != 'y':
-        print('Open the game {}, then try again.'.format(game_title))
+    if game_instance.is_response_yes(response):
+        response_message = game_instance.get_retry_message_when_game_is_closed()
+        print(response_message)
         return
 
-    print('Watch me play {}.'.format(game_title))
+    response_message = game_instance.get_start_message_when_game_is_open()
+    print(response_message)
 
 if __name__ == '__main__':
     main()
